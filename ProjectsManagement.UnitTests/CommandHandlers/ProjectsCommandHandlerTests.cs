beaaed5a0f4411e8cac5b6200cfc93289d26b66c
mohamad.alturky <m799678@gmail.com>
2024-08-20 18:01:22 +0300

    adding unit testing

---
+++ ProjectsManagement.UnitTests/CommandHandlers/ProjectsCommandHandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace ProjectsManagement.UnitTests.CommandHandlers;
+
+[TestFixture]
+public class ProjectsCommandHandlerTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void GetCustomers_ShouldReturnAListOfCustomers_WhenCallingIt()
+    {
+        int x = 1;
+        Assert.That(x, Is.EqualTo(1));
+    }
+}
